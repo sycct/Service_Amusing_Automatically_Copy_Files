2031c58fd8a878cc9d4551bc3a4f2105b0af855e
sycct <huang_9119596@163.com>
2022-04-26 16:47:14 +0800

    2022-4-26 complete conpy file.

---
+++ automatically_copy_file_main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from datetime import datetime, timedelta
+import os
+from apscheduler.schedulers.blocking import BlockingScheduler
+from dotenv import load_dotenv
+
+from utils import file_util
+
+
+class AutomaticallyCopyFiles(object):
+    def __init__(self):
+        # loading env config file
+        dotenv_path = os.path.join(os.getcwd(), '.env')
+        if os.path.exists(dotenv_path):
+            load_dotenv(dotenv_path)
+
+        self._base_path = os.environ.get('BASE_PATH')
+        self._init_file = file_util.FileUtil()
+
+    def copy_files_run(self):
+        # 计算一分钟以前的时间
+        time_different = datetime.utcnow() - timedelta(minutes=1)
+        # 按照时间到排序的文件列表
+        file_list = self._init_file.get_file_list(self._base_path)
+        # 需要复制的文件列表
+        copy_file_list = []
+        for file in file_list:
+            # 获取当前文件
+            file = os.path.getctime(os.path.join(self._base_path, file))
+            # 当前文件创建时间，将时间戳转换成时间
+            get_file_crate_time = datetime.fromtimestamp(file)
+            if get_file_crate_time > time_different:
+                # 如果是一分钟以内新建的文件加入文件列表
+                copy_file_list.append(file)
+        # 将需要复制的文件复制到远程服务器
+        if copy_file_list:
+            self._init_file.copy_file(copy_file_list, self._base_path)
+
+
+if __name__ == "__main__":
+    scheduler = BlockingScheduler(timezone="Asia/Shanghai")
+    copy_file = AutomaticallyCopyFiles()
+    # 星期一早上 5：30：30 运行 automatically_update 自动更新
+    scheduler.add_job(copy_file.copy_files_run, 'interval', minute='1')
+    # 开始运行调度器
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        exit(0)
--- utils/file_util.py
+++ utils/file_util.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 import os
-from datetime import datetime
+import paramiko
 
 
 class FileUtil(object):
@@ -15,11 +15,33 @@ class FileUtil(object):
         if not dir_list:
             return
         else:
-            for item in dir_list:
-                print(datetime.fromtimestamp(os.path.getctime(os.path.join(file_path, item))))
             # 注意，这里使用lambda表达式，将文件按照最后修改时间顺序升序排列
             # os.path.getmtime() 函数是获取文件最后修改时间
             # os.path.getctime() 函数是获取文件最后创建时间
             dir_list = sorted(dir_list, key=lambda x: os.path.getctime(os.path.join(file_path, x)), reverse=True)
             # print(dir_list)
             return dir_list
+
+    @staticmethod
+    def copy_file(file_list, base_path):
+        """
+        两台服务器之间同步文件
+        :param file_list: 文件名列表
+        :param base_path: 远程文件路径
+        :return:
+        """
+        remote_host = os.environ.get('TARGET_HOSTNAME')
+        remote_user = os.environ.get('TARGET_USER')
+        remote_password = os.environ.get('TARGET_PASSWORD')
+        remote_port = int(os.environ.get('TARGET_PROT'))
+        for item in file_list:
+            ssh = paramiko.SSHClient()
+            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            ssh.connect(remote_host, username=remote_user, password=remote_password, port=remote_port)
+            sftp = ssh.open_sftp()
+            # 本地文件路径
+            local_file_path = os.path.join(base_path, item)
+            # 由于远程文件与本地文件一样，所以目录也是一样的
+            sftp.put(localpath=local_file_path, remotepath=local_file_path)
+            sftp.close()
+            ssh.close()
