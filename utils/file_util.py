a80d24d3c91e9be7b6652ee9648b3532b92ecd1b
sycct <huang_9119596@163.com>
2022-04-26 15:40:03 +0800

    Initial commit

---
+++ utils/file_util.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import os
+from datetime import datetime
+
+
+class FileUtil(object):
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def get_file_list(file_path):
+        dir_list = os.listdir(file_path)
+        if not dir_list:
+            return
+        else:
+            for item in dir_list:
+                print(datetime.fromtimestamp(os.path.getctime(os.path.join(file_path, item))))
+            # 注意，这里使用lambda表达式，将文件按照最后修改时间顺序升序排列
+            # os.path.getmtime() 函数是获取文件最后修改时间
+            # os.path.getctime() 函数是获取文件最后创建时间
+            dir_list = sorted(dir_list, key=lambda x: os.path.getctime(os.path.join(file_path, x)), reverse=True)
+            # print(dir_list)
+            return dir_list
